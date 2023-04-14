9775d61fe1ddc3fcfb41799c5f14ff943930111c
Rimsky <Rimsky@DESKTOP-BO2IQFC.lan>
2023-04-13 23:32:19 -0400

    "Test 2..."

---
+++ src/new_Test_2_package/New_Test_2_Class.java
@@ -0,0 +1,13 @@
+package new_Test_2_package;
+
+public class New_Test_2_Class {
+
+	
+	public static void main (String args []) {
+		System.out.println("Testing...");
+	}
+	
+	public void testMessage() {
+		System.out.println("Test 2...");
+	}
+}
